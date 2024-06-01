22297072013d9481d9b8d81031c6b8347f6746c7
Albert Aptacy <albertaptacy@gmail.com>
2024-06-01 10:22:44 +0200

    function added

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
-const form = document.querySelector(".form")
-const output = document.querySelector(".form__output");
+function calculate() {
+    const form = document.querySelector(".form")
+    const output = document.querySelector(".form__output");
 
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const selectElement = document.querySelector(".js-form-select");
+        const selectedOption = selectElement.selectedOptions[0];
+        const nameAttribute = selectedOption.getAttribute("name");
+        const exchangeRate = document.querySelector(".js-form-select").value;
+        const amount = document.querySelector(".js-form-input").value;
+        const calculatedValue = (amount * exchangeRate).toFixed(2);
+        output.innerHTML = `${amount} ${nameAttribute} = ${calculatedValue} PLN`;
+    });
+}
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const selectElement = document.querySelector(".js-form-select");
-    const selectedOption = selectElement.selectedOptions[0];
-    const nameAttribute = selectedOption.getAttribute("name");
-    const exchangeRate = document.querySelector(".js-form-select").value;
-    const amount = document.querySelector(".js-form-input").value;
+calculate();
 
-    const calculatedValue = (amount * exchangeRate).toFixed(2);
-
-    output.innerHTML = `${amount} ${nameAttribute} = ${calculatedValue} PLN`;
-});
